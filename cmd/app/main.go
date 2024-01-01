7e596897edbecf6501e5a1b5866b05d2270ea853
Mikhail Kostin <a@a.ru>
2024-01-01 16:42:09 +0300

    added tokens-list

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,25 @@
 package main
 
-import "indicators-monitor/internal/watcher"
+import (
+	"indicators-monitor/internal/watcher"
+	"os"
+	"strings"
+)
 
 func main() {
 	block := make(chan bool)
-	watcher.NewWatcher([]string{
-		"ETH", "BTC", "DOGE", "SOL",
-		"BNB", "ADA", "SAND", "MATIC",
-		"OP",
-	}).Start()
+	// watcher.NewWatcher([]string{
+	// 	"ETH", "BTC", "DOGE", "SOL",
+	// 	"BNB", "ADA", "SAND", "MATIC",
+	// 	"OP",
+	// }).Start()
+
+	watcher.NewWatcher(readTokens())
 
 	<-block
+}
+
+func readTokens() []string {
+	f, _ := os.ReadFile("configs/tokens-list")
+	return strings.Split(string(f), "\n")
 }
